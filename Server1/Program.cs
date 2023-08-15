8904c669cd302b4ac297070a5f4d94ad6dedc935
akmal4163 <bebekngayang@gmail.com>
2023-08-16 00:00:46 +0700

    Add project files.

---
+++ Server1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+
+namespace Server1
+{
+    public class main
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Socket socket;
+                int port = 8000;
+                Server1.Server myserver = new Server1.Server(port);
+                myserver.Create();
+                Console.WriteLine("server started");
+
+                
+            } catch (Exception e)
+            {
+                Console.WriteLine("cannot create a web server", e.ToString());
+            }
+        }
+    }
+}
---
+++ Server1/Server.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server1
+{
+    public class Server
+    {
+        Socket socket;
+        int port;
+        IPHostEntry host;
+        IPAddress address;
+        IPEndPoint localendpoint;
+
+        public Server(int port)
+        {
+            this.port = port;
+            this.host = Dns.GetHostEntry("localhost");
+            this.address = host.AddressList[0];
+            this.localendpoint = new IPEndPoint(address, this.port);
+        }
+        public void Create()
+        {
+            Socket server_socket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            server_socket.Bind(localendpoint);
+
+            server_socket.Listen(10);
+
+            Console.WriteLine("waiting for a connection");
+            Socket client_socket = server_socket.Accept();
+
+            String data = null;
+            Byte[] bytes = null;
+
+            while (true)
+            {
+                bytes = new byte[1024];
+                int bytes_received = client_socket.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytes_received);
+                Console.WriteLine(data);
+                if(data.Contains("GET"))
+                {
+                    string currentDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                    string response = "<html><h1>Hello from C# server</h1><p>Method: GET</p><p>Current date and time: " + currentDate + "</p></html>";
+
+                    string httpResponse = "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: text/html\r\n"
+                        + "Content-Length: " + response.Length + "\r\n"
+                        + "\r\n"
+                        + response;
+
+                    //String senderdata =;
+                    try
+                    {
+                        byte[] messagetoclient = Encoding.ASCII.GetBytes(httpResponse);
+                        client_socket.Send(messagetoclient);
+                        Console.WriteLine("sending data success");
+                    }
+                    catch
+                    {
+                        Console.Write(httpResponse);
+                    }
+
+                }
+
+                if (data.IndexOf("<EOF>") > -1)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("bytes received: ", bytes, "data :", data);
+
+            client_socket.Shutdown(SocketShutdown.Both);
+            client_socket.Close();
+        }
+    }
+}
